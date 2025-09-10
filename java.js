0b9b0be112f7f2c3d0588b870cb511170b4d8d22
Favour Blessing <favour@example.com>
2025-09-10 09:45:46 +0100

    Remove unrelated files, keep only password generator project

--- java.js
+++
@@ -1,8 +0,0 @@
-// Simple JavaScript Example
-function addNumbers(a, b) {
-  return a + b;
-}
-
-console.log(addNumbers(5, 10));   // 15
-console.log(addNumbers("5", 10)); // "510"  (string + number)
-
--- test.js
+++
@@ -1,9 +0,0 @@
-// test.js
-function addNumbers(a, b) {
-  return a + b;
-}
-
-console.log(addNumbers(5, 10));
-console.log(addNumbers("5", 10));
-
-
